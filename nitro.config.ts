1f954cb8e270449ca0ffb98cd4331aa5981edb8e
emanh <cnttblog@gmail.com>
2025-06-16 14:09:22 +0700

    resolve @ alias

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,9 @@
+import { resolve } from 'node:path';
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   compatibilityDate: '2025-06-16',
-  srcDir: "server"
+  srcDir: "server",
+  alias: {
+    '@': resolve(__dirname, 'server'),
+  }
 });
