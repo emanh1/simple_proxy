195e9466c2e6bcc6d8b73b79c8a04f385be3ac6c
emanh <cnttblog@gmail.com>
2025-07-12 22:45:23 +0700

    add version

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,12 @@
 import { resolve } from 'node:path';
+import pkg from './package.json';
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   compatibilityDate: '2025-06-16',
   srcDir: "server",
+  runtimeConfig: {
+    version: pkg.version
+  },
   alias: {
     '@': resolve(__dirname, 'server'),
   }
